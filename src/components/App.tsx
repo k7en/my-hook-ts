b96db68628428f4dea3b71da296fcf51f6c3ec45
kensuzuki <kenichi_c_suzuki@ricoh-japan.co.jp>
2019-11-18 15:53:01 +0900

    remove theme

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,56 +1,16 @@
 import React, { createContext, useContext } from 'react';
 import './App.css';
 import Counter from './Counter'
-const themes = {
-  light: {
-    foreground: "#000000",
-    background: "#eeeeee"
-  },
-  dark: {
-    foreground: "#ffffff",
-    background: "#222222"
-  }
-};
 
-const ThemeContext = React.createContext(themes.light)
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <ThemeContext.Provider value={themes.dark}>
-          <Toolbar />
-        </ThemeContext.Provider>
         < Counter />
-
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
           Learn React
-        </a>
       </header>
     </div>
-  );
-}
-
-function Toolbar() {
-  return (
-    <div>
-      <ThemedButton />
-    </div>
-  );
-}
-
-function ThemedButton() {
-  const theme = useContext(ThemeContext);
-  console.log("theme" + JSON.stringify(theme))
-  return (
-    <button style={{ background: theme.background, color: theme.foreground }}>
-      I am styled by theme context!
-    </button>
   );
 }
 
